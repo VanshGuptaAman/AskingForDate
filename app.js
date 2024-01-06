9365ce8846dd294857e737b796cb6d7ec54c57de
Vansh Gupta <vanshgupta1608@gmail.com>
2024-01-07 03:43:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function sendEmail() {
 }
 
 b.addEventListener('mouseover', function() {
-    const i = Math.floor(Math.random() * 500) + 1 ;
+    const i = Math.floor(Math.random() * 900) + 1 ;
     const j = Math.floor(Math.random() * 500) + 1 ;
     b.style.left = i + 'px';
     b.style.top = j + 'px';
